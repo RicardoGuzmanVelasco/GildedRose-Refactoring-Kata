75b42d5b7f09f76c71c6986dcd3e921c65ba2063
RGV <ricardo.guzman.velasco@outlook.com>
2025-08-05 11:03:54 +0200

    Revert everything from the last kata

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -1,45 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿namespace GildedRoseKata;
 
-namespace GildedRoseKata;
-
-public abstract class Item
+public class Item
 {
-    public virtual string Name { get; private init; }
-    public int SellIn { get; protected set; }
-    public int Quality { get; protected set; }
-
-    public static Item Create(string name, int sellIn, int quality)
-    {
-        return name switch
-        {
-            "Sulfuras, Hand of Ragnaros" => new Sulfuras() { SellIn = sellIn, Quality = quality },
-            "Aged Brie" => new AgedBrie() { SellIn = sellIn, Quality = quality },
-            "Backstage passes to a TAFKAL80ETC concert" => new BackstagePasses() { SellIn = sellIn, Quality = quality },
-            "Conjured Mana Cake" => new Conjured() { SellIn = sellIn, Quality = quality},
-            _ => new Normal { Name = name, SellIn = sellIn, Quality = quality }
-        };
-    }
-
-    public abstract void PassDay();
-
-    protected void DecreaseSellIn()
-    {
-        SellIn -= 1;
-    }
-
-    protected bool IsSpoiled()
-    {
-        return SellIn < 0;
-    }
-
-    protected void DecreaseQuality()
-    {
-        Quality = Math.Max(0, Quality - 1);
-    }
-
-    protected void IncreaseQuality()
-    {
-        Quality = Math.Min(50, Quality + 1);
-    }
+    public string Name { get; set; }
+    public int SellIn { get; set; }
+    public int Quality { get; set; }
 }
--- csharp.NUnit/GildedRose/Program.cs
+++ csharp.NUnit/GildedRose/Program.cs
@@ -11,16 +11,31 @@ public class Program
 
         IList<Item> items = new List<Item>
         {
-            Item.Create("+5 Dexterity Vest", 10, 20),
-            Item.Create("Aged Brie", 2, 0),
-            Item.Create("Elixir of the Mongoose", 5, 7),
-            Item.Create("Sulfuras, Hand of Ragnaros", 0, 80),
-            Item.Create("Sulfuras, Hand of Ragnaros", -1, 80),
-            Item.Create("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            Item.Create("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            Item.Create("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 15,
+                Quality = 20
+            },
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 10,
+                Quality = 49
+            },
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 5,
+                Quality = 49
+            },
             // this conjured item does not work properly yet
-            Item.Create("Conjured Mana Cake", 3, 6)
+            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
         };
 
         var app = new GildedRose(items);
