86bce07017496e51f5bbf471346a1a26f32746bc
guillermo-ggl <148863785+guillermo-ggl@users.noreply.github.com>
2025-08-12 11:56:12 +0200

    refactor: encapsulate PassDay in item class

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -15,77 +15,9 @@ public class GildedRose
     {
         foreach (var item in Items)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (QualityNotSulfurasdojderfgoijsdofg(item))
-                {
-                    item.Quality--;
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality--;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
+            item.PassDay();
         }
     }
 
-    private static bool QualityNotSulfurasdojderfgoijsdofg(Item item)
-    {
-        return item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros";
-    }
+
 }
--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -6,4 +6,81 @@ public class Item
     public int SellIn { get; set; }
     public int Quality { get; set; }
     
+    public void PassDay()
+    {
+        if (Name != "Aged Brie" && Name != "Backstage passes to a TAFKAL80ETC concert")
+        {
+            if (QualityNotSulfurasdojderfgoijsdofg())
+            {
+                Quality--;
+            }
+        }
+        else
+        {
+            if (Quality < 50)
+            {
+                Quality++;
+
+                if (Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (SellIn < 11)
+                    {
+                        if (Quality < 50)
+                        {
+                            Quality++;
+                        }
+                    }
+
+                    if (SellIn < 6)
+                    {
+                        if (Quality < 50)
+                        {
+                            Quality++;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (Name != "Sulfuras, Hand of Ragnaros")
+        {
+            SellIn = SellIn - 1;
+        }
+
+        if (SellIn < 0)
+        {
+            if (Name != "Aged Brie")
+            {
+                if (Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Quality > 0)
+                    {
+                        if (Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Quality--;
+                        }
+                    }
+                }
+                else
+                {
+                    Quality = Quality - Quality;
+                }
+            }
+            else
+            {
+                if (Quality < 50)
+                {
+                    Quality++;
+                }
+            }
+        }
+    }
+    
+        
+
+    private bool QualityNotSulfurasdojderfgoijsdofg()
+    {
+        return Quality > 0 && Name != "Sulfuras, Hand of Ragnaros";
+    }
+    
 }
